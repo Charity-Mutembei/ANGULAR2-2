54606e7afcb4c4b1b39fd6faee5ddd52acf984f4
Charity-Mutembei <charity.mutembei@student.moringaschool.com>
2022-04-25 21:24:54 +0300

    Feat: Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GitProfileComponent } from './git-profile/git-profile.component';
+import { GitRepositoriesComponent } from './git-repositories/git-repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "git-profile", component:GitProfileComponent}, 
+  {path: "git-repositories", component: GitRepositoriesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
